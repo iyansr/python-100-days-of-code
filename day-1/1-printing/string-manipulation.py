22d044aa66248c43d3a7674904cd57b150f9262f
iyansr <ikhyan7@gmail.com>
2020-11-19 01:38:15 +0700

    day-1 : printing

---
+++ day-1/1-printing/string-manipulation.py
@@ -0,0 +1,7 @@
+print("Hello" + " " + "Iyan")
+print("Hello\nIyan")
+print(" ")
+
+##
+
+print("Day 1 - String Manipulation\nString Concatenation is done with the \"+\" sign.\ne.g. print(\"Hello \" + \"world\")\nNew lines can be created with a backslash and n.")
